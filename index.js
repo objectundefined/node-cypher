8ca87571806503cf5dbcf30bb28761ee8dcf3bb8
objectundefined <gabriel.lipson@gmail.com>
2014-02-21 18:13:52 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var CypherClient = require('./lib/CypherClient');
+var request = require('request');
+var _ = require('underscore');
+
+exports.createClient = createClient = function ( hostOrConnOpts , clientOptsOrCb, cbOrNull ) {
+  
+  var self = this ;
+  var httpOpts = self._httpOpts = _.isString(hostOrConnOpts) ? { host : hostOrConnOpts } : hostOrConnOpts || {} ;
+  var clientOpts = _.isFunction(clientOptsOrCb) ? {} : clientOptsOrCb || {} ;
+  var cb = _.isFunction(clientOptsOrCb) ? clientOptsOrCb : cbOrNull||function(){} ; 
+  
+  _.defaults(httpOpts,{
+    host : 'http://localhost:7474',
+    base : '/db/data'
+  });
+  
+  request.get({
+    json : true,
+    url : httpOpts.host + httpOpts.base
+  },function(err,result,json){
+    if (err) {
+      return cb(err);
+    } else if (json.cypher && json.batch) {
+      _.extend(httpOpts,{
+        cypher : json.cypher.split(httpOpts.base)[1],
+        batch : json.batch.split(httpOpts.base)[1]
+      });
+      cb(null,new CypherClient(httpOpts,clientOpts));
+    } else {
+      return cb(new Error('Cypher and Batch not available'))
+    }
+  })
+  
+}
+
+createClient('http://localhost:7474', {batchMax:100},function(err,client){
+  client.query('MATCH (p:Person) RETURN p',{},function(){
+    console.log('here',arguments)
+  })
+})
